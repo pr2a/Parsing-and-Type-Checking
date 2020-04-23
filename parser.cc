fa2ce8934aa6cb02c394961f5cd04439b9b1edac
pr2a <31780665+pr2a@users.noreply.github.com>
2020-04-22 19:01:49 -0700

    Working parser

--- parser.cc
+++ parser.cc
@@ -14,7 +14,7 @@
 using namespace std;
 
 void Parser::syntax_error(){
-    cout << "SYNTAX ERROR !!!\n";
+    cout << "Syntax Error &!#@";
     exit(1);
 }
 
@@ -42,7 +42,49 @@ void Parser::parse_scope(){
 }
 
 void Parser::parse_scope_list(){
-   
+    Token t = lexer.GetToken();
+    if(t.token_type == LBRACE){
+        lexer.UngetToken(t);
+        parse_scope();
+        t = peek();
+        if(t.token_type == LBRACE || t.token_type == ID || t.token_type == WHILE){
+            parse_scope_list();
+        }
+    }
+    else if(t.token_type == ID){
+        Token t2 = lexer.GetToken();
+        if(t2.token_type == COMMA || t2.token_type == COLON){
+            lexer.UngetToken(t2);
+            lexer.UngetToken(t);
+            parse_var_decl();
+            t = peek();
+            if(t.token_type == LBRACE || t.token_type == ID || t.token_type == WHILE){
+                parse_scope_list();
+            }
+        }
+        else if(t2.token_type == EQUAL){
+            lexer.UngetToken(t2);
+            lexer.UngetToken(t);
+            parse_stmt();
+            t = peek();
+            if(t.token_type == LBRACE || t.token_type == ID || t.token_type == WHILE){
+                parse_scope_list();
+            }
+        }
+        else
+            syntax_error();
+    }
+    else if(t.token_type == WHILE){
+        lexer.UngetToken(t);
+        parse_stmt();
+        Token t = peek();
+        if(t.token_type == LBRACE || t.token_type == ID || t.token_type == WHILE){
+            parse_scope_list();
+        }
+    }
+    else{
+        syntax_error();
+    }
 }
 
 void Parser::parse_var_decl(){
@@ -56,6 +98,7 @@ void Parser::parse_id_list(){
     expect(ID);
     Token t = peek();
     if(t.token_type == COMMA){
+        expect(COMMA);
         parse_id_list();
     }
 }
@@ -70,12 +113,14 @@ void Parser::parse_type_name(){
         expect(BOOLEAN);
     else if(t.token_type == STRING)
         expect(STRING);
+    else
+        syntax_error();
 }
 
 void Parser::parse_stmt_list(){
     parse_stmt();
     Token t = peek();
-    if(t.token_type == ID || WHILE){
+    if(t.token_type == ID || t.token_type == WHILE){
         parse_stmt_list();
     }
 }
@@ -88,6 +133,8 @@ void Parser::parse_stmt(){
     else if(t.token_type == WHILE){
         parse_while_stmt();
     }
+    else
+        syntax_error();
 }
 
 void Parser::parse_assign_stmt(){
@@ -99,81 +146,74 @@ void Parser::parse_assign_stmt(){
 
 void Parser::parse_while_stmt(){
     expect(WHILE);
+    expect(LPAREN);
+    parse_condition();
+    expect(RPAREN);
     Token t = peek();
-    if(t.token_type == LPAREN){
-        expect(LPAREN);
-        parse_condition();
-        expect(RPAREN);
+    if(t.token_type == LBRACE){
         expect(LBRACE);
         parse_stmt_list();
         expect(RBRACE);
     }
-    else{
-        parse_condition();
+    else if(t.token_type == ID || t.token_type == WHILE){
         parse_stmt();
     }
+    else
+        syntax_error();
 }
 
 void Parser::parse_expr(){
     Token t = peek();
-    //same ones: NUM, REALNUM, STRING_CONSTANT, GREATER, GTEQ, LESS, NOTEQUAL, LTEG
-    if(t.token_type == ID || t.token_type == NUM || t.token_type == REALNUM || t.token_type == STRING_CONSTANT){
+    if(t.token_type == ID || t.token_type == NUM || t.token_type == REALNUM || t.token_type == STRING_CONSTANT || t.token_type == TRUE || t.token_type == FALSE){
         parse_primary();
     }
-    else if(t.token_type == GREATER || t.token_type == GTEQ || t.token_type == LESS || t.token_type == NOTEQUAL || t.token_type == LTEQ || t.token_type == ID || t.token_type == NUM || t.token_type == REALNUM || t.token_type == STRING_CONSTANT){
+    else if(t.token_type == PLUS || t.token_type == MINUS || t.token_type == MULT || t.token_type == DIV){
         parse_arithmetic_expr();
     }
     else if(t.token_type == AND || t.token_type == OR || t.token_type == XOR || t.token_type == GREATER || t.token_type == GTEQ || t.token_type == LESS || t.token_type == NOTEQUAL || t.token_type == LTEQ || t.token_type == NOT){
         parse_boolean_expr();
     }
-        
+    else
+        syntax_error();
 }
 
 void Parser::parse_arithmetic_expr(){
-        Token t = peek();
-        if(t.token_type == GREATER || t.token_type == GTEQ || t.token_type == LESS || t.token_type == NOTEQUAL || t.token_type == LTEQ){
-            parse_arithmetic_operator();
-            parse_arithmetic_expr();
-            parse_arithmetic_expr();
-        }
-        else if(t.token_type == ID || t.token_type == NUM || t.token_type == REALNUM || t.token_type == STRING_CONSTANT){
-            parse_arithmetic_primary();
-        }
+    Token t = peek();
+    if(t.token_type == PLUS || t.token_type == MINUS || t.token_type == MULT || t.token_type == DIV){
+        parse_arithmetic_operator();
+        parse_arithmetic_expr();
+        parse_arithmetic_expr();
+    }
+    else if(t.token_type == ID || t.token_type == NUM || t.token_type == REALNUM || t.token_type == STRING_CONSTANT){
+        parse_arithmetic_primary();
+    }
 }
 
 void Parser::parse_boolean_expr(){
     Token t = peek();
-    //binary_boolean_expr
+    //binary_boolean_op
     if(t.token_type == AND || t.token_type == OR || t.token_type == XOR){
         parse_binary_boolean_operator();
         parse_boolean_expr();
         parse_boolean_expr();
     }
     //relational_operator
-    Token t2 = lexer.GetToken();
-    if(t2.token_type == GREATER || t2.token_type == GTEQ || t2.token_type == LESS || t2.token_type == NOTEQUAL || t2.token_type == LTEQ){
-        Token t3 = lexer.GetToken();
-        //arithmetic_expr
-        if(t3.token_type == PLUS || t3.token_type == MINUS || t3.token_type == MULT || t3.token_type == DIV || t3.token_type == ID || t3.token_type == NUM || t3.token_type == REALNUM || t3.token_type == STRING_CONSTANT){
-            lexer.UngetToken(t3);
-            lexer.UngetToken(t2);
-            parse_relational_operator();
-            parse_arithmetic_expr();
-            parse_arithmetic_expr();
-        }
-        //boolean_expr
-        else{
-            parse_relational_operator();
-            parse_boolean_expr();
-            parse_boolean_expr();
-        }
-            
+    else if(t.token_type == GREATER || t.token_type == GTEQ || t.token_type == LESS || t.token_type == NOTEQUAL || t.token_type == LTEQ){
+        parse_relational_operator();
+        parse_expr();
+        parse_expr();
     }
     //NOT
     else if(t.token_type == NOT){
         expect(NOT);
         parse_boolean_expr();
     }
+    //boolean_primary
+    else if(t.token_type == ID || t.token_type == TRUE || t.token_type == FALSE){
+        parse_boolean_primary();
+    }
+    else
+        syntax_error();
 }
 
 void Parser::parse_arithmetic_operator(){
@@ -186,6 +226,8 @@ void Parser::parse_arithmetic_operator(){
         expect(MULT);
     else if(t.token_type == DIV)
         expect(DIV);
+    else
+        syntax_error();
 }
 
 void Parser::parse_binary_boolean_operator(){
@@ -196,6 +238,8 @@ void Parser::parse_binary_boolean_operator(){
         expect(OR);
     else if(t.token_type == XOR)
         expect(XOR);
+    else
+        syntax_error();
 }
 
 void Parser::parse_relational_operator(){
@@ -210,6 +254,8 @@ void Parser::parse_relational_operator(){
         expect(NOTEQUAL);
     else if(t.token_type == LTEQ)
         expect(LTEQ);
+    else
+        syntax_error();
 }
 
 void Parser::parse_primary(){
@@ -224,6 +270,8 @@ void Parser::parse_primary(){
         expect(STRING_CONSTANT);
     else if(t.token_type == TRUE || t.token_type == FALSE)
         parse_bool_const();
+    else
+        syntax_error();
 }
 
 void Parser::parse_arithmetic_primary(){
@@ -236,6 +284,8 @@ void Parser::parse_arithmetic_primary(){
         expect(REALNUM);
     else if(t.token_type == STRING_CONSTANT)
         expect(STRING_CONSTANT);
+    else
+        syntax_error();
 }
 
 void Parser::parse_boolean_primary(){
@@ -244,6 +294,8 @@ void Parser::parse_boolean_primary(){
         expect(ID);
     else if(t.token_type == TRUE || t.token_type == FALSE)
         parse_bool_const();
+    else
+        syntax_error();
 }
 
 void Parser::parse_bool_const(){
@@ -252,8 +304,10 @@ void Parser::parse_bool_const(){
         expect(TRUE);
     else if(t.token_type == FALSE)
         expect(FALSE);
+    else
+        syntax_error();
 }
 
 void Parser::parse_condition(){
-    parse_expr();
+    parse_boolean_expr();
 }
